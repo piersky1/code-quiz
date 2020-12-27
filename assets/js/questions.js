d0efc7166f5f69ca4f78264f1f244871a4f56496
Pierce Brelinsky <p.brelinsky@gmail.com>
2020-12-27 02:28:14 -0500

    Created questions template.

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        title: "Question #1?",
+        choices: ["A", "B", "C", "D"],
+        answer: "X"
+    },
+
+    {
+        title: "Question #2?",
+        choices: ["A", "B", "C", "D"],
+        answer: "X"
+    },
+
+    {
+        title: "Question #3?",
+        choices: ["A", "B", "C", "D"],
+        answer: "X"
+    },
+
+    {
+        title: "Question #4",
+        choices: ["A", "B", "C", "D"],
+        answer: "X"
+    },
+
+    {
+        title: "Question #5?",
+        choices: ["A", "B", "C", "D"],
+        answer: "X"
+    },
+
+];
